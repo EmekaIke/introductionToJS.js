08274df847d509df05539d97e101157176fcc272
Emeka Ike <70453914+EmekaIke@users.noreply.github.com>
2022-06-21 16:33:44 +0100

    Add files via upload

---
+++ introductionToJS.js
@@ -0,0 +1,7 @@
+let name = "Emeka Ike"
+let height = "7 Feet"
+let country = "Nigeria"
+
+console.log("My name is " +name)
+console.log("My height is " +height)
+console.log("My country is " +country)
